69b13e2b750c06d82e84e2012a6b4e84843630ad
Alexander North <anorth@apple.com>
2019-07-18 12:00:54 -0700

    Add changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM python:2.7
-MAINTAINER Shekhar Gulati "shekhargulati84@gmail.com"
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
-
+EXPOSE 80
 CMD ["app.py"]
--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,7 +1,28 @@
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
 
 app_labels = {"app": "flask"}
+image = "123094825799.dkr.ecr.us-west-2.amazonaws.com/test-github-actions/flask-test:latest"
+flask_container = \
+    {
+        "name": "flask-app",
+        "image": image,
+        "ports":
+            [{"containerPort": 8080}]
+    }
+ecr_pull_secret = \
+    {
+        "name": "regcred"
+    }
+
+spec = \
+    {
+        "containers":
+            [flask_container],
+        "imagePullSecrets":
+            [ecr_pull_secret]
+    }
 
 deployment = Deployment(
     "flask",
@@ -10,25 +31,19 @@ deployment = Deployment(
         "replicas": 1,
         "template": {
             "metadata": {"labels": app_labels},
-            "spec": {
-                "containers":
-                    [
-                        {
-                            "name":
-                                "flask-app",
-                            "image":
-                                "123094825799.dkr.ecr.us-west-2.amazonaws.com/test-github-actions/flask-test:latest",
-                            "ports":
-                                [
-                                    {
-                                        "containerPort": 8080
-                                    }
-                                ]
-
-                        }
-                    ]
-            }
+            "spec": spec
         }
+    })
+
+frontend = Service(
+    "flask",
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "LoadBalancer",
+        "ports": [{"port": 80, "target_port": 80, "protocol": "TCP"}],
+        "selector": app_labels,
     })
 
 pulumi.export("name", deployment.metadata["name"])
